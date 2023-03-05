a716e3daa11b1c7756942c4b2d3814ad5bb5b617
Adrian333Dev <adrian333dev@gmail.com>
2023-03-05 06:22:11 +0400

    Inserting Nodes into a Linked List

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node
 {
@@ -7,18 +8,23 @@ typedef struct node
 } Node;
 
 void print_list(Node *head);
+Node *insert_at_head(Node *head, int new_value);
+Node *insert_at_tail(Node *head, int new_value);
 
 int main()
 {
-  Node a, b, c;
-  a.value = 2;
-  b.value = 3;
-  c.value = 4;
-  a.next = &b;
-  b.next = &c;
-  c.next = NULL;
+  Node *list1_head = NULL;
 
-  print_list(&a);
+  // list1_head = insert_at_head(list1_head, 7);
+  // list1_head = insert_at_head(list1_head, 5);
+  // list1_head = insert_at_head(list1_head, 3);
+
+  list1_head = insert_at_tail(list1_head, 10);
+  list1_head = insert_at_tail(list1_head, 20);
+  list1_head = insert_at_tail(list1_head, 30);
+
+  print_list(list1_head);
+  free(list1_head);
 }
 
 void print_list(Node *head)
@@ -31,5 +37,36 @@ void print_list(Node *head)
     printf("Node %i: %i\n", i, current->value);
     i++;
     current = current->next;
+  }
+}
+
+Node *insert_at_head(Node *head, int new_value)
+{
+  Node *new_node = calloc(1, sizeof(Node));
+  new_node->value = new_value;
+
+  if (head == NULL)
+    return new_node;
+  else
+  {
+    new_node->next = head;
+    return new_node;
+  }
+}
+
+Node *insert_at_tail(Node *head, int new_value)
+{
+  Node *new_node = calloc(1, sizeof(Node));
+  new_node->value = new_value;
+
+  if (head == NULL)
+    return new_node;
+  else
+  {
+    Node *current = head;
+    while (current->next != NULL)
+      current = current->next;
+    current->next = new_node;
+    return head;
   }
 }
